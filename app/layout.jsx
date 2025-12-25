119a922a5494f248c35d0b59bb81f943b330a534
Your Name <mayukh1995@gmail.com>
2025-12-25 14:39:14 +0530

    ...

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,20 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-33LCZ27G7X"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-33LCZ27G7X');
+            `}
+          </Script>
+        </head>
         <body className={inter.className}>
           <NextSSRPlugin
             routerConfig={extractRouterConfig(ourFileRouter)}
